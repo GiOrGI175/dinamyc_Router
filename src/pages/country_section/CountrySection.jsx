11467660e933413d802e5e8957fca11ef23e9262
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2024-08-15 20:20:51 +0400

    feature fetch

--- src/pages/country_section/CountrySection.jsx
+++ src/pages/country_section/CountrySection.jsx
@@ -1,12 +1,63 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
+import styles from './CountrySection.module.scss';
 import FilterBtn from '../../components/header/filterButton/FilterBtn';
 
 const CountrySection = () => {
+  const [countrys, setCountrys] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch('https://restcountries.com/v3.1/all');
+        if (!res.ok) {
+          throw new Error('Network error');
+        }
+        const data = await res.json();
+        setCountrys(data);
+        setLoading(false);
+      } catch (error) {
+        console.log('error data', error);
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  console.log(countrys);
+
   return (
-    <div>
+    <main>
       <FilterBtn />
-    </div>
+      <div className={styles.main_container}>
+        <div className={styles.main_content}>
+          {loading ? (
+            <p>Loading...</p>
+          ) : (
+            <div>
+              {countrys.map((country) => {
+                return (
+                  <div>
+                    <div>
+                      <img src={country.flags.png} alt='flag' />
+                    </div>
+                    <div>
+                      <h2>{country.name.common}</h2>
+                      <div>
+                        <p>Population: {country.population}</p>
+                        <p>Region: {country.region}</p>
+                        <p>Capital: {country.capital}</p>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          )}
+        </div>
+      </div>
+    </main>
   );
 };
 
