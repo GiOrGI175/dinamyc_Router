cfb93fcc35902f7df20126bee9c1614a75a954a8
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2024-08-17 02:15:29 +0400

    c

--- src/pages/country_section/CountrySection.jsx
+++ src/pages/country_section/CountrySection.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
 import styles from './CountrySection.module.scss';
-import FilterBtn from '../../components/filterButton/FilterBtn';
 
 const CountrySection = () => {
   const [countrys, setCountrys] = useState();
